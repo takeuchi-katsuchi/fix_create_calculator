17df3230645d470fd25c5b8c67f317d362a65725
takeuchikatsuchi <k64katu195@gmail.com>
2020-06-14 12:39:56 +0900

    CORS stttings

--- calculator/settings.py
+++ calculator/settings.py
@@ -35,6 +35,7 @@ INSTALLED_APPS = [
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
+    'corsheaders',
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
@@ -44,6 +45,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -123,4 +125,9 @@ STATIC_URL = '/front/'
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "front"),
+)
+
+
+CORS_ORIGIN_WHITELIST = (
+    'http://localhost:8000',
 )
